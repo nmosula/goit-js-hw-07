bd5e60935f9a261193132dddd9c5465c892cada6
Nazar Mosula <nmosula@gmail.com>
2022-12-12 20:44:59 +0200

    Update task-02 (lightbox)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce((acc, {
+    preview,
+    original,
+    description
+}) => acc + `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`, '');
+
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+
+galleryList.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {
+    evt.preventDefault();
+  
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });  
+
+}
